5c9349fc437d6da77ef17d7982d90878dfc9abe2
Westley Case <wcaseaz@gmail.com>
2015-03-10 11:29:07 -0600

    Psudo code for index_of function

---
+++ Project_06/Project_06/Driver.cpp
@@ -0,0 +1,23 @@
+/*
+*
+*
+*/
+#include "Driver.h"
+
+
+int main()
+{
+
+
+	system("pause");
+	return 0;
+}
+
+// non recursive but will call its recursive helper function.
+int index_of(string s, string t)
+{
+	// locate the first instance of the string t within the string s
+	// return the index of the starting position in s where this substring occurs.
+
+	// if not found, handle it and return -1 for the calling method to handle
+}
---
+++ Project_06/Project_06/Driver.h
@@ -0,0 +1,9 @@
+/*
+* Driver.h
+*/
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int index_of(string, string);
