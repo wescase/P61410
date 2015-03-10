b89fdcfe043b992bb97dd31fbb1a55620b439ccc
Morgan <morgandallred123@gmail.com>
2015-03-10 13:42:01 -0600

    Wes Turn

--- Project_06/Project_06/Driver.cpp
+++ Project_06/Project_06/Driver.cpp
@@ -7,17 +7,69 @@
 
 int main()
 {
+	string str1;
+	string str2;
+
+	cout << "This Program will find the occurence of one string inside another." << endl << endl;
+	cout << "Enter the string to be searched:  ";
+	cin >> str1;
+	cin.ignore(0);
+
+	cout << "Now enter the string you want to search for:  ";
+	cin >> str2;
+	cin.ignore(0);
 
 
-	system("pause");
-	return 0;
+	int idx = index_of(str1, str2);
+
+	if (idx == -ERROR_ONE)
+	{
+		cout << "String 2 must be shorter in length than string 1" << endl;
+		system("pause");
+		return 0;
+	}
+	if (idx == -ERROR_TWO)
+	{
+		cout << "The string was not found." << endl;
+		system("pause");
+		return 0;
+	}
+	else
+	{
+		cout << "The index of substring is = " << idx << endl;
+		system("pause");
+		return 0;
+	}
 }
 
 // non recursive but will call its recursive helper function.
 int index_of(string s, string t)
 {
+	if (s.length() < t.length())
+		return -ERROR_ONE;
 	// locate the first instance of the string t within the string s
 	// return the index of the starting position in s where this substring occurs.
-
 	// if not found, handle it and return -1 for the calling method to handle
+	return index_ofH(s, t, 0);
 }
+
+
+int index_ofH(string str1, string str2, int i)
+{
+
+	if ((i + str2.length()) > str1.length())
+	{
+		return -ERROR_TWO;
+	}
+
+	if (str1.substr(i, str2.length()) == str2)
+	{
+		return i;
+	}
+	else
+	{
+		i++;
+		return index_ofH(str1, str2, i);
+	}
+}
+
--- Project_06/Project_06/Driver.h
+++ Project_06/Project_06/Driver.h
@@ -6,4 +6,13 @@
 
 using namespace std;
 
+const int ERROR_ONE = -1;
+const int ERROR_TWO = -2;
+
+
+// Purpose: To find a string within a second string
+// Parameters: String 1 (string to look in) String 2 (string to find)
+// Returns: Int (the index of the starting char of String 2
 int index_of(string, string);
+
+int index_ofH(string, string, int);
